48e6dc711f02881977033d05229a9cecc8ad2b2c
TK <teaseungkim@gmail.com>
2020-12-30 09:22:13 -0600

    setup for psql db

---
+++ db/config.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { Pool } = require('pg');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DEV_DATABASE}`;
+
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
+});
+
+// Used to see if I could insert in psql
+// pool.query(
+//   "INSERT INTO student(firstname, lastname, age, address, email)VALUES('Mary Ann', 'Wilters', 20, '74 S Westgate St', 'mroyster@royster.com')",
+//   (err, res) => {
+//     console.log(err, res);
+//     pool.end();
+//   }
+// );
+
+module.exports = { pool };
--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { pool } from './db/config';
 
 const app = express();
-const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+// app.get('/api/greeting', (req, res) => {
+//   const name = req.query.name || 'World';
+//   res.setHeader('Content-Type', 'application/json');
+//   client
+//   .query(`SELECT * FROM student`, values)
+//   .then(res => {
+//     console.log(res.rows[0])
+//   })
+//   .catch(e => console.error(e.stack))
+//   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+// });
+
+// Start server
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`Server listening`);
 });
