7f4eb84ebc582d23d10b15b80ea27f1fd2e626a4
TK <teaseungkim@gmail.com>
2020-12-30 21:12:28 -0600

    finished making seed.js file

--- db/seed.js
+++ db/seed.js
@@ -4,33 +4,114 @@ import { parse } from 'fast-csv';
 import db from './config';
 
 const csvDir = path.join(__dirname + '/arkcsv/');
-const arketfs = ['ARKK', 'ARKQ', 'ARKW', 'ARKG', 'ARKF'];
+const arketfs = ['arkk', 'arkq', 'arkw', 'arkg', 'arkf'];
 
 // Drop and create tables then insert rows
-const dropTableQuery = `DROP TABLE IF EXISTS ${arketfs[0]}, ${arketfs[1]}, ${arketfs[2]}, ${arketfs[3]}, ${arketfs[4]}`;
-// drop tables
-db.none(dropTableQuery)
-  .then(res => {
-    // Create tables
-    console.log('DROPPED TABLES...');
-    const arketfTablesPromises = arketfs.map(etf => {
-      const createTableQuery = `CREATE TABLE ${etf} (id INT GENERATED ALWAYS AS IDENTITY, date DATE, fund VARCHAR, company VARCHAR, ticker VARCHAR, cusip VARCHAR, shares BIGINT, market_value NUMERIC(2), weight NUMERIC(2))`;
-      return db.none(createTableQuery);
+async function dropTables() {
+  const dropTableQuery = `DROP TABLE IF EXISTS arkk, arkq, arkw, arkg, arkf`;
+  try {
+
+    await db.none(dropTableQuery)
+      .catch(err => {
+        throw err;
+      })
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+async function createTables() {
+  const createTableArkfQuery = `CREATE TABLE arkf (date TEXT, fund VARCHAR, company VARCHAR, ticker VARCHAR, cusip VARCHAR, shares NUMERIC(40,2), market_value NUMERIC(40,2), weight NUMERIC(5,2))`;
+  const createTableArkgQuery = `CREATE TABLE arkg (date TEXT, fund VARCHAR, company VARCHAR, ticker VARCHAR, cusip VARCHAR, shares NUMERIC(40,2), market_value NUMERIC(40,2), weight NUMERIC(5,2))`;
+  const createTableArkkQuery = `CREATE TABLE arkk (date TEXT, fund VARCHAR, company VARCHAR, ticker VARCHAR, cusip VARCHAR, shares NUMERIC(40,2), market_value NUMERIC(40,2), weight NUMERIC(5,2))`;
+  const createTableArkqQuery = `CREATE TABLE arkq (date TEXT, fund VARCHAR, company VARCHAR, ticker VARCHAR, cusip VARCHAR, shares NUMERIC(40,2), market_value NUMERIC(40,2), weight NUMERIC(5,2))`;
+  const createTableArkwQuery = `CREATE TABLE arkw (date TEXT, fund VARCHAR, company VARCHAR, ticker VARCHAR, cusip VARCHAR, shares NUMERIC(40,2), market_value NUMERIC(40,2), weight NUMERIC(5,2))`;
+  try {
+    await db.none(createTableArkfQuery)
+      .catch(err => {
+        throw err;
+      })
+    await db.none(createTableArkgQuery)
+      .catch(err => {
+        throw err;
+      })
+    await db.none(createTableArkkQuery)
+      .catch(err => {
+        throw err;
+      })
+    await db.none(createTableArkqQuery)
+      .catch(err => {
+        throw err;
+      })
+    await db.none(createTableArkwQuery)
+      .catch(err => {
+        throw err;
+      })
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+async function copyCSVs() {
+  const copyArkfQuery = `COPY Arkf from '${csvDir}Arkf.csv' DELIMITER ',' CSV HEADER`;
+  const copyArkgQuery = `COPY Arkg from '${csvDir}Arkg.csv' DELIMITER ',' CSV HEADER`;
+  const copyArkkQuery = `COPY Arkk from '${csvDir}Arkk.csv' DELIMITER ',' CSV HEADER`;
+  const copyArkqQuery = `COPY Arkq from '${csvDir}Arkq.csv' DELIMITER ',' CSV HEADER`;
+  const copyArkwQuery = `COPY Arkw from '${csvDir}Arkw.csv' DELIMITER ',' CSV HEADER`;
+  try {
+    await db.none(copyArkfQuery)
+      .then(data => {
+        console.log(`Copied: Arkf`)
+      })
+      .catch(err => {
+        throw err
+      })
+    await db.none(copyArkgQuery)
+      .then(data => {
+        console.log(`Copied: Arkg`)
+      })
+      .catch(err => {
+        throw err
+      });
+    await db.none(copyArkkQuery)
+      .then(data => {
+        console.log(`Copied: Arkk`)
+      })
+      .catch(err => {
+        throw err
+      });
+    await db.none(copyArkqQuery)
+      .then(data => {
+        console.log(`Copied: Arkq`)
+      })
+      .catch(err => {
+        throw err
+      });
+    await db.none(copyArkwQuery)
+      .then(data => {
+        console.log(`Copied: Arkw`)
+      })
+      .catch(err => {
+        throw err
+      });
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+async function runSeed() {
+  await dropTables()
+    .then(() => {
+      console.log('*****DROPPED TABLES*****')
     });
-    return Promise.all(arketfTablesPromises);
-  })
-  .then(res => {
-    // insert rows
-    // headers for csv --> date,fund,company,ticker,cusip,shares,"market value($)",weight(%)
-    console.log('CREATED TABLES...');
-    arketfs.forEach(etf => {
-      const copyQuery = `COPY ${etf} FROM ‘${csvDir}${etf}’`
-      db.one(copyQuery)
-        .on('error', error => console.error(error))
-        .on('data', data => console.log(data))
-        .on('end', rowCount => console.log(rowCount));
+  await createTables()
+    .then(() => {
+      console.log('*****CREATED TABLES*****')
     });
-  })
-  .catch(err => {
-    return err;
-  });
+  await copyCSVs()
+    .then(() => {
+      console.log('*****FINISHED*****')
+    });
+}
+
+runSeed();
